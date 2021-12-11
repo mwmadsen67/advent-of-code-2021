6fc590ab91935317f4f23dc55dbaf6e3e6171d2a
Mike Madsen <mikewmadsen@gmail.com>
2021-12-10 22:56:35 -0800

    figure out day 11

---
+++ day11/part1.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+let codeInput = fs.readFileSync('day11/puzzleInput.txt', 'utf8').split('\n').map(el => el.split('').map(num => parseInt(num)));
+// console.log(codeInput)
+let flashes = 0;
+for (let n = 0; n < 100; n++) {
+  let mem = {};
+  for (let i = 0; i < codeInput.length; i++) {
+    for (let j = 0; j < codeInput[i].length; j++) {
+      let num = codeInput[i][j];
+      if (mem[`${i},${j}`] === true) {
+        continue;
+      } else if (num < 9) {
+        codeInput[i][j] += 1;
+        continue;
+      } 
+      codeInput[i][j] = 0;
+
+      let queue = [`${i},${j}`];
+      mem[`${i},${j}`] = true;
+      
+      while (queue.length > 0) {
+        let cur = queue.pop();
+        let [x,y] = cur.split(',').map(el => parseInt(el));
+        flashes++;
+        for (let k = -1; k < 2; k++) {
+          for (let l = -1; l < 2; l++) {
+            if (codeInput[x+k] !== undefined && codeInput[x+k][y+l] !== undefined && mem[`${x+k},${y+l}`] !== true) {
+              codeInput[x+k][y+l] += 1;
+              if (codeInput[x+k][y+l] > 9) {
+                codeInput[x+k][y+l] = 0;
+                mem[`${x+k},${y+l}`] = true;
+                queue.push(`${x+k},${y+l}`);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+console.log(codeInput.join('\n'));
+
+console.log(flashes)
